79c53af3e66c0586e73dbd88d18546e098b2aec7
saasbook <saasbook@saasbook.(none)>
2012-09-30 16:27:40 -0700

    sun afternoon 4:30 pm

--- hw2/localee/app/models/user.rb
+++ hw2/localee/app/models/user.rb
@@ -6,23 +6,24 @@ class User < ActiveRecord::Base
 
   # Use Rails internal password digesting
   has_secure_password
-  
+
   # Validations
   validates :name, :presence => true
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true, :on => :create, :length => {:minimum => 5}
-  
+
   ############################################
   #  TODO: you may optionally add code here  #
   ############################################
-  has_many :posts, :inverse_of => user
+  has_many :posts #, :inverse_of => user
 
-  def to_hash
-    {
-      :id => self.id,
-      :name => self.name,
-      :email => self.email
-    }
-  end
+ # def to_hash
+  #  puts "to_hash happening now!"
+   # {
+    #  :id => self.id,
+     # :name => self.name,
+      #:email => self.email
+  #  }
+#  end
 
 end
