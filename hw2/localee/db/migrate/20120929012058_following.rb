4e3e6138559b30fe4e3cd2ffc58e0e993ccb6ce5
saasbook <saasbook@saasbook.(none)>
2012-09-30 21:06:35 -0700

    follow locations working

--- hw2/localee/db/migrate/20120929012058_following.rb
+++
@@ -1,8 +0,0 @@
-class Following < ActiveRecord::Migration
-  def change
-    create_table :following, :id => false do |t|
-      t.integer :user_id # foreign key for user
-      t.integer :location_id # foreign key for location
-    end
-  end
-end
--- hw2/localee/db/schema.rb
+++ hw2/localee/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120929012058) do
+ActiveRecord::Schema.define(:version => 20121001033249) do
 
   create_table "following", :id => false, :force => true do |t|
     t.integer "user_id"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(:version => 20120929012058) do
     t.float    "longitude"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "locations_users", :id => false, :force => true do |t|
+    t.integer "location_id"
+    t.integer "user_id"
   end
 
   create_table "posts", :force => true do |t|
